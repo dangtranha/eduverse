26f26b65e1b5d17a0b9a3e16d37b7c5ce65ecc13
minhnhat1206 <minhnhatnhc@gmail.com>
2024-11-17 15:13:18 +0700

    overviewcourse va card

--- eduverse/src/App.jsx
+++ eduverse/src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import HomePage from "./View/HomePage";
+import DiscoverPage from "./view/Discoverpage";
+import OverviewCourse from "./Component/OverviewCourse";
 function App() {
   return (
     <div className="App">
