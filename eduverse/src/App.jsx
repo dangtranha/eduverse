e3201806eaf0047f4a429ebb35c663336621dbfa
Quoc Huy <quochuy.working@gmail.com>
2024-11-19 04:42:58 +0700

    ok

--- eduverse/src/App.jsx
+++ eduverse/src/App.jsx
@@ -1,26 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AuthView from "./view/AuthView";
-import CourseContentView from "./view/CourseContentView";
-import TestPage from "./view/TestPage";
+
 
 import HomePage from "./view/HomePage";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AuthView from "./view/AuthView";
-import CourseContentView from "./view/CourseContentView";
-import TestPage from "./view/TestPage";
 
 // import CourseContentView from "./view/CourseContentView";
 import Login from "./view/LoginView"
 // import CardControler from "./controller/CardController";
 
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+
 
 function App() {
   return (
--- eduverse/src/view/LoginView.jsx
+++ eduverse/src/view/LoginView.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { login } from "../controller/AuthController";
+// import { login } from "../controller/AuthController";
 import ComponentLogin from "../component/LoginForm";
 
 const Login = () => {
